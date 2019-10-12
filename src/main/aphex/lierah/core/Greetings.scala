09a72e56a7de097a291289cebedc6fb1e3ba5baa
Adrian Nuhacet <dheriannewat88@gmail.com>
2019-10-12 15:44:53 +0200

    Update project configuration with base routing endpoint

--- src/main/aphex/lierah/core/Greetings.scala
+++
@@ -1,5 +0,0 @@
-package aphex.lierah.core
-
-case class Greetings(name: String) {
-  def greet(): String = "Hello"
-}
---
+++ src/test/aphex/lierah/core/AphexApiTest.scala
@@ -0,0 +1,18 @@
+package aphex.lierah.core
+
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.concurrent.ScalaFutures
+
+final class AphexApiTest extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
+  "AphexApi" should {
+    "respond successfully while requesting its status" in {
+      Get("/status") ~> Routes.all ~> check {
+        status shouldBe StatusCodes.OK
+        contentType shouldBe ContentTypes.`application/json`
+        entityAs[String] shouldBe """{"status":"ok"}"""
+      }
+    }
+  }
+}
