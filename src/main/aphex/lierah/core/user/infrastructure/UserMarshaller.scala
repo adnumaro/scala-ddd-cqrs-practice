de7deaa2fdf24971dda3c43ad8aefec60351ae1d
Adrian Nuhacet <dheriannewat88@gmail.com>
2019-10-12 17:03:53 +0200

    Marshall User class instances with Spray-Json

---
+++ src/main/aphex/lierah/core/user/infrastructure/UserMarshaller.scala
@@ -0,0 +1,40 @@
+package aphex.lierah.core.user.infrastructure
+
+import java.util.UUID
+
+import spray.json.{DeserializationException, JsonFormat, JsString, JsValue, RootJsonFormat}
+import spray.json.DefaultJsonProtocol._
+
+import aphex.lierah.core.user.domain.{User, UserId, UserName}
+
+object UserMarshaller {
+
+  implicit object UserNameMarshaller extends JsonFormat[UserName] {
+    def write(value: UserName): JsValue = JsString(value.name)
+
+    def read(value: JsValue): UserName = value match {
+      case JsString(name) => UserName(name)
+      case _              => throw DeserializationException("Expected 1 string for UserName")
+    }
+  }
+
+  implicit object UuidMarshaller extends JsonFormat[UUID] {
+    def write(value: UUID): JsValue = JsString(value.toString)
+
+    def read(value: JsValue): UUID = value match {
+      case JsString(uuid) => UUID.fromString(uuid)
+      case _              => throw DeserializationException("Expected hexadecimal UUID string")
+    }
+  }
+
+  implicit object UserIdMarshaller extends JsonFormat[UserId] {
+    def write(value: UserId): JsValue = JsString(value.value.toString)
+
+    def read(value: JsValue): UserId = value match {
+      case JsString(id) => UserId(id)
+      case _            => throw DeserializationException("Expected 1 string for UserId")
+    }
+  }
+
+  implicit val userFormat: RootJsonFormat[User] = jsonFormat2(User.apply(_: UserId, _: UserName))
+}
