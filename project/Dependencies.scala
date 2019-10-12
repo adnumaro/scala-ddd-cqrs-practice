a552109089514276cf12a1bc03482cd599346f8f
Adrian Nuhacet <dheriannewat88@gmail.com>
2019-10-12 15:16:01 +0200

    Update code style

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,13 +9,13 @@ object Dependencies {
 
   val production = Seq(
     "com.github.nscala-time" %% "nscala-time" % "2.18.0",
-    "com.lihaoyi" %% "pprint" % "0.5.3",
-    "com.typesafe.akka" %% "akka-http" % Versions.akkaHttp
+    "com.lihaoyi"            %% "pprint"      % "0.5.3",
+    "com.typesafe.akka"      %% "akka-http"   % Versions.akkaHttp
   )
 
   val test = Seq(
-    "org.scalatest" %% "scalatest" % "3.0.4" % Test,
-    "com.typesafe.akka" %% "akka-testkit" % Versions.akka % Test,
+    "org.scalatest"     %% "scalatest"         % "3.0.4"           % Test,
+    "com.typesafe.akka" %% "akka-testkit"      % Versions.akka     % Test,
     "com.typesafe.akka" %% "akka-http-testkit" % Versions.akkaHttp % Test
   )
 }
