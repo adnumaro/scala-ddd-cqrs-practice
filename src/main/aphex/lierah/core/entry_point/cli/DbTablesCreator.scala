eac0c56a6ef439f031641bb60f3ecbc7bde5f84d
Adrian Nuhacet <dheriannewat88@gmail.com>
2019-10-13 16:12:47 +0200

    Add db table creator command

---
+++ src/main/aphex/lierah/core/entry_point/cli/DbTablesCreator.scala
@@ -0,0 +1,77 @@
+package aphex.lierah.core.entry_point.cli
+
+import java.io.File
+import java.sql.{Connection, DriverManager}
+
+import scala.util.matching.Regex
+
+import com.typesafe.config.ConfigFactory
+import scala.io.Source._
+import scala.util.Try
+
+import aphex.lierah.core.module.shared.infrastructure.config.DbConfig
+
+/**
+  * View usage: runMain aphex.lierah.core.entry_point.cli.DbTablesCreator --usage
+  * Execute: runMain aphex.lierah.core.entry_point.cli.DbTablesCreator
+  */
+object DbTablesCreator {
+  private val databaseNameFromUrlRegex = new Regex("""\w+:\w+:\/\/\d+.\d+.\d+.\d+(?::\w+)?\/(\w+)""")
+
+  case class CommandConfig(
+      tablesFolder: String = "database",
+      configFile: String = "application",
+      dbConfigKey: String = "database"
+  )
+
+  def main(args: Array[String]): Unit = {
+    val parser = new scopt.OptionParser[CommandConfig]("DbTablesCreator") {
+      head("Build needed environment to run tests", "1.0")
+      opt[String]('f', "tablesFolder")
+        .action((tablesFolder, currentConfig) => currentConfig.copy(tablesFolder = tablesFolder))
+        .text("Folder containing all the `.sql` files with the `CREATE TABLE` definitions.")
+
+      opt[String]('c', "configFile")
+        .action((configFile, currentConfig) => currentConfig.copy(configFile = configFile))
+        .text("Configuration file to use while searching for the DB connection parameters.")
+
+      opt[String]('k', "dbConfigKey")
+        .action((dbConfigKey, currentConfig) => currentConfig.copy(dbConfigKey = dbConfigKey))
+        .text("The configuration key inside the configuration file where we'll find the DB connection parameters.")
+    }
+
+    parser.parse(args, CommandConfig()).fold(println("[ERROR] Invalid parameters")) { commandConfig =>
+      val dbConfig     = DbConfig(ConfigFactory.load("application").getConfig("database"))
+      val dbNameOption = for (grouped <- databaseNameFromUrlRegex findFirstMatchIn dbConfig.host) yield grouped group 1
+
+      dbNameOption.fold(
+        println(s"[ERROR] We couldn't extract the DB name from the DB URL configuration parameter: ${dbConfig.host}")
+      ) { dbName =>
+        Try(Class.forName(dbConfig.driver)).toOption.fold(
+          println(s"[ERROR] Invalid driver specified in the database config: ${dbConfig.driver}")
+        ) { _ =>
+          val connection = DriverManager.getConnection(dbConfig.host, dbConfig.user, dbConfig.password)
+
+          createTables(commandConfig.tablesFolder, connection)
+
+          connection.close()
+        }
+      }
+    }
+  }
+
+  private def createTables(tablesFolder: String, connection: Connection): Unit = {
+    val tablesFolderFile = new File(tablesFolder)
+    val tablesFiles      = tablesFolderFile.listFiles()
+
+    println(s"[INFO] Creating the following tables: ${tablesFiles.mkString(", ")}…")
+
+    val createTablesQueries = tablesFiles.map(fromFile(_).getLines.mkString)
+
+    val applySchemaStatement = connection.createStatement
+
+    // Add specific query before for each create table query
+    applySchemaStatement.executeUpdate(s"")
+    createTablesQueries.foreach(applySchemaStatement.executeUpdate)
+  }
+}
