9e51180735295276c728d2477c32a5b71467678f
Adrian Nuhacet <dheriannewat88@gmail.com>
2019-10-21 21:53:13 +0200

    Update password validation pattern

--- src/main/aphex/lierah/core/module/user/domain/value_object/UserPassword.scala
+++ src/main/aphex/lierah/core/module/user/domain/value_object/UserPassword.scala
@@ -3,7 +3,7 @@ package aphex.lierah.core.module.user.domain.value_object
 import aphex.lierah.core.module.shared.domain.Encrypter
 
 object UserPassword {
-  val Pattern = "(?=.*?)[a-zA-Z0-9@#&\\_-]{8,30}"
+  val Pattern = "(?=.*?)[\\p{Alnum}\\p{Punct}]{8,30}"
 
   def apply(plainPassword: String): UserPasswordHash = plainPassword match {
     case plainPassword if plainPassword.matches(Pattern) =>
